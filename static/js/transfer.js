92895562c1096c09ee7299c28df483b376fa45b5
venyy <2815458556@qq.com>
2023-04-08 14:56:11 +0800

    add

---
+++ static/js/transfer.js
@@ -0,0 +1,16 @@
+// 导航栏实现页面跳转
+function turnTopic() {
+    top.location.href = "http://127.0.0.1:5000/topicPage"
+}
+
+function turnHotSearch() {
+    top.location.href = "http://127.0.0.1:5000/hotSearchPage"
+}
+
+function turnHome() {
+    top.location.href = "http://127.0.0.1:5000/"
+}
+
+function turnOthers() {
+    top.location.href = "http://127.0.0.1:5000/othersPage"
+}
